05482fe27d3b31bb56f8123ea0baa3bd4013845b
David Manners <dmanners87@gmail.com>
2017-02-10 18:13:33 +0000

    Add very very basic getter for product data

--- app/Catalog/Category/CategoryList.php
+++ app/Catalog/Category/CategoryList.php
@@ -2,11 +2,22 @@
 
 namespace App\Catalog\Category;
 
+use Moltin\SDK\Facade\Moltin as Moltin;
+use Moltin\SDK\Facade\Product as Product;
+
 class CategoryList
 {
     public function run($request, $response, $args)
     {
-        $response->write("Hello world");
+        // Authenticate credentials
+        Moltin::Authenticate('ClientCredentials', [
+            'client_id'     => getenv('MOLTIN_CLIENT_ID'),
+            'client_secret' => getenv('MOLTIN_CLIENT_SECRET')
+        ]);
+
+        $product = Product::Get(1447270639613248266);
+
+        $response->write($product['result']['sku']);
 
         return $response;
     }

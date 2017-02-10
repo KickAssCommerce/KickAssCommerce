4ad50241ae6d25d1d292121597b0698b10fda403
David Manners <dmanners87@gmail.com>
2017-02-10 22:49:28 +0000

    Add basic search function and expand the product interface

--- app/Application/ProductInterface.php
+++ app/Application/ProductInterface.php
@@ -5,4 +5,6 @@ namespace App\Application;
 interface ProductInterface
 {
     public function getProductList(array $filter = array());
+    public function getProductItem(int $identifier);
+    public function getSearchResults(array $terms = array());
 }

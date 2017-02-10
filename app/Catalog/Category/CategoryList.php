95c32b2cf91ce769b2656f7e1068940af96e0555
David Manners <dmanners87@gmail.com>
2017-02-10 17:32:55 +0000

    Fix the namespace to include the app from the composer.json

--- app/Catalog/Category/CategoryList.php
+++ app/Catalog/Category/CategoryList.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Catalog\Category;
+namespace App\Catalog\Category;
 
 class CategoryList
 {
--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ use Slim\App;
 
 $app = new App();
 
-$app->get('/shop/category/list', 'Catalog\Category\CategoryList::run');
+$app->get('/shop/category/list', 'App\Catalog\Category\CategoryList::run');
 
 $app->run();

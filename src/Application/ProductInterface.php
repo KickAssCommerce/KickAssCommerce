0f55186b46724da34caaae46a24c7358d36e16eb
Sander Mangel <sander@sandermangel.nl>
2017-02-12 01:58:14 +0100

    added getProductItemByAttribute to product interface

--- src/Application/ProductInterface.php
+++ src/Application/ProductInterface.php
@@ -4,6 +4,7 @@ namespace KickAss\Commerce\Application;
 
 interface ProductInterface
 {
-    public function getProductList(array $filter = array());
+    public function getProductList(array $filter = []);
     public function getProductItem(int $identifier);
+    public function getProductItemByAttribute(string $attribute, string $value);
 }
